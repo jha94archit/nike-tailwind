3344390ce63b08996846a8691f0c865314bb7d8e
Archit Jha <architjha@Archits-MacBook-Pro.local>
2023-09-29 01:47:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {
+  Hero,
+  Nav,
+  PopularProducts,
+  SpecialProduct,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews,
+  Footer,
+} from "./index";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SpecialProduct />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Hero from "./sections/Hero";
+import Nav from "./components/Nav";
+import PopularProducts from "./sections/PopularProducts";
+import SpecialProduct from "./sections/SpecialProduct";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+
+export {
+  Hero,
+  Nav,
+  PopularProducts,
+  SpecialProduct,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  CustomerReviews,
+  Footer,
+};
